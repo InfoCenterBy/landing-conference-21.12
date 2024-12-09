a84987df841ff39bc0cfb67aa0d4768e3d31c07e
dyamorph <dyamorph22@gmail.com>
2024-12-09 17:19:14 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,71 +1,68 @@
 (() => {
-     'use strict';
+  "use strict";
 
-     const forms = document.querySelectorAll('.needs-validation');
+  const forms = document.querySelectorAll(".needs-validation");
 
-     Array.from(forms).forEach((form) => {
-          form.addEventListener(
-               'submit',
-               (event) => {
-                    if (!form.checkValidity()) {
-                         event.preventDefault();
-                         event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-               },
-               false
-          );
-     });
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
 })();
-const burger = document.getElementById('burger');
-const menu = document.getElementById('menu');
+
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
 if (burger) {
-     burger.addEventListener('click', () => {
-          burger.classList.toggle('active');
-          menu.classList.toggle('active');
-     });
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
 }
 
-
-document
-     .querySelector('#collapseProgrammBtn')
-     .addEventListener('click', (e) => {
-          if (!e.target.classList.contains('collapsed')) {
-               e.target.innerHTML = 'Свернуть';
-          } else {
-               e.target.innerHTML = 'Читать полностью';
-          }
-     });
-
-const swiper = new Swiper('.photo-slider-main.swiper', {
-     // Optional parameters
-     direction: 'horizontal',
-     loop: true,
-     pagination: {
-          el: '.photo-slider-main .swiper-pagination',
-          type: 'fraction',
-     },
-     slidesPerView: 1,
-     // Navigation arrows
-     navigation: {
-          nextEl: '.photo-slider-main .swiper-button-next',
-          prevEl: '.photo-slider-main .swiper-button-prev',
-     },
-     centeredSlides: true,
-     effect: 'coverflow',
-     coverflowEffect: {
-          rotate: 0,
-          stretch: 60,
-          depth: 200,
-          // modifier: 2,
-          slideShadows: true,
-          scale: 0.9,
-     },
-     initialSlide: 1,
-     breakpoints: {
-          1000: {
-               slidesPerView: 2,
-          },
-     },
+document.querySelector("#collapseProgrammBtn").addEventListener("click", (e) => {
+  if (!e.target.classList.contains("collapsed")) {
+    e.target.innerHTML = "Свернуть";
+  } else {
+    e.target.innerHTML = "Читать полностью";
+  }
 });
 
+const swiper = new Swiper(".photo-slider-main.swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".photo-slider-main .swiper-pagination",
+    type: "fraction",
+  },
+  slidesPerView: 1,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".photo-slider-main .swiper-button-next",
+    prevEl: ".photo-slider-main .swiper-button-prev",
+  },
+  centeredSlides: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 60,
+    depth: 200,
+    // modifier: 2,
+    slideShadows: true,
+    scale: 0.9,
+  },
+  initialSlide: 1,
+  breakpoints: {
+    1000: {
+      slidesPerView: 2,
+    },
+  },
+});
